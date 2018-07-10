cfe54e316a6c50da62ebe15e2623a5302cf819e0
小狼 <1131389540@qq.com>
2018-07-10 10:51:03 +0800

    Initial commit

---
+++ app/src/main/java/social/milin/tech/begin/MainActivity.java
@@ -0,0 +1,18 @@
+package social.milin.tech.begin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tt = findViewById(R.id.tt);
+        tt.setText("大家好");
+    }
+}
